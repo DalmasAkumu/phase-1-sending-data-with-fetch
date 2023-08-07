2ee9120a431dc0621e1edb42e4cb18f1f7b06350
Dalmas <akumuodhiambo@gmail.com>
2023-08-07 10:48:41 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(userName,userEmail){
+ 
+    const url ='http://localhost:3000/users'
+    const userData = {name: userName, email: userEmail}
+    const postData = {
+        method : "POST",
+        headers : {
+            "content-type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify(userData)
+        }
+
+        fetch(url,postData)
+            .then(response => response.json())
+            .then(resData => 
+                document.body.innerHTML = resData.id
+                )
+            .catch(error => {
+                document.body.innerHTML = error.message
+            })
+}
+
+submitData("leah","leahochi@yahoo.com")
